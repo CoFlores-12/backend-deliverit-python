7c9b5ea6c3719aa04dac8ccbd6763deff86feec4
CoFlores-12 <obethflores2014@gmail.com>
2023-07-12 00:44:51 -0400

    categories & stores

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
+import json
+from bson import json_util
+from bson.objectid import ObjectId
 
 load_dotenv()
 
@@ -10,7 +13,7 @@ USR = os.getenv("USR")
 PW = os.getenv("PW")
 
 app = Flask(__name__)
-app.run(debug=True)
+app.run(port=3000,debug=True)
 
 uri = "mongodb+srv://"+USR+":"+PW+"@deliverit.elvsi2t.mongodb.net/?retryWrites=true&w=majority"
 
@@ -57,4 +60,34 @@ def sigin():
 
         x = mycol.insert_one(data)
         return "Inserted"
+    return "Error"
+
+@app.route("/categories", methods=['GET'])
+def categories():
+    mycol = mydb["categories"]
+    fd =  mycol.find()
+    return json.loads(json_util.dumps(fd))
+
+@app.route("/categories/<idCategory>", methods=['GET'])
+def categoriesID(idCategory):
+    if len(str(idCategory)) > 0:
+        mycol = mydb["categories"]
+        fd =  mycol.find({"_id": ObjectId(idCategory)})
+        return json.loads(json_util.dumps(fd))
+    return "Error"
+
+@app.route("/stores/<idCategory>", methods=['GET'])
+def storescategoriesID(idCategory):
+    if len(str(idCategory)) > 0:
+        mycol = mydb["stores"]
+        fd =  mycol.find({"category.id": ObjectId(idCategory)})
+        return json.loads(json_util.dumps(fd))
+    return "Error"
+
+@app.route("/store/<idStore>", methods=['GET'])
+def storeID(idStore):
+    if len(str(idStore)) > 0:
+        mycol = mydb["stores"]
+        fd =  mycol.find({"_id": ObjectId(idStore)})
+        return json.loads(json_util.dumps(fd))
     return "Error"
